5cfcf1a26840953b683dcb7b54f50d23949d45ea
Luis David Cruz Perez <ldcruz@scio.local>
2019-03-21 12:33:03 -0600

    Friend Modal as Component

--- src/components/friends/friends.js
+++ src/components/friends/friends.js
@@ -1,30 +1,24 @@
 import React, { Component } from 'react';
 import FriendCard from '../shared-components/friend-card/friend-card.js';
 import './friends.scss'
-import {Modal, Button, Image}  from 'react-bootstrap';
+import ModalFriend from '../shared-components/modal/modal.js'
 
 class Friends extends Component {
-
     constructor(props, context) {
         super(props, context);
-
+    
         this.handleShow = this.handleShow.bind(this);
-        this.handleClose = this.handleClose.bind(this);
-
+    
         this.state = {
-            show: false,
-            name: "Juan",
+          show: false,
+          name: "",
+          image: ""
         };
-    }
-
-    handleClose() {
-        this.setState({ show: false });
-    }
-
+      }
     handleShow(nombre, imagen) {
         this.setState({ show: true, name: nombre, image: imagen });
     }
-
+      
     render() {
 
         let friends = [
@@ -85,23 +79,12 @@ class Friends extends Component {
         return (
             <div className="container">
                 <div className="row justify-content-around justify-content-xs-center align-items-center">
-                    {<Modal show={this.state.show} onHide={this.handleClose}>
-                        <Modal.Header closeButton>
-                            <Modal.Title>{this.state.name}</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body><Image src={this.state.image} rounded width="100%" /></Modal.Body>
-                        <Modal.Footer>
-                            <Button variant="secondary" onClick={this.handleClose}>
-                                Close
-                </Button>
-                        </Modal.Footer>
-                    </Modal>}
+                    {<ModalFriend modal={this.state.show} name={this.state.name} image={this.state.image}/>}
                     {
                         friends.map((item, idx) =>
                             <div key={idx} className="col-lg-2 col-md-4 col-sm-6 col-xs-12" onClick={() => this.handleShow(item.name, item.image)}>
                                 <FriendCard friend={item} />
                             </div>
-
                         )
                     }
                 </div>
--- src/components/shared-components/modal/modal.js
+++ src/components/shared-components/modal/modal.js
@@ -1,50 +1,42 @@
 import React from 'react';
-import {Modal, Button, Image}  from 'react-bootstrap';
-import {connect} from 'react-redux';
-
+import { Modal, Button, Image } from 'react-bootstrap';
 
 class ModalFriend extends React.Component {
-    constructor(props, context) {
-        super(props, context);
-    
-        this.handleShow = this.handleShow.bind(this);
-        this.handleClose = this.handleClose.bind(this);
-    
-        this.state = {
-          show: false,
-          name: "Juan",
-          image: "vacio",
-        };
-      }
+  constructor(props, context) {
+    super(props, context);
+    this.handleClose = this.handleClose.bind(this);
+    this.state = {
+      show: false,
+      name: "Juan",
+      image: "vacio",
+    };
+  }
 
-      handleClose() {
-        this.setState({ show: false });
-      }
-    
-      handleShow(nombre, imagen) {
-        this.setState({ show: true, name: nombre, image: imagen });
-    }
-    
-    render() {
-        return (
-          <>
-            <Modal show={this.state.show} onHide={this.handleClose} handleShow={this.handleShow}>
-              <Modal.Header closeButton>
-                <Modal.Title>Modal heading</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-              <Modal.Footer>
-                <Button variant="secondary" onClick={this.handleClose}>
-                  Close
-                </Button>
-                <Button variant="primary" onClick={this.handleClose}>
-                  Save Changes
-                </Button>
-              </Modal.Footer>
-            </Modal>
-          </>
-        );
-    }
+  componentWillReceiveProps() {
+    this.setState({show: this.props.modal})
+  }
+  
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  render() {
+    return (
+      <> 
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>{this.props.name}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body><Image src={this.props.image} rounded width="100%" /></Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              Close
+          </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
 }
 
-export default ModalFriend(connect(this.handleShow));
+export default ModalFriend;
