32981582b55728510c07ecd957e800ce774824a7
Jonathan Sanchez Ochoa <jsanchez@scio.local>
2019-03-21 10:19:06 -0600

    removed comments, removed global colors

--- src/components/shared-components/add-button/add-button.js
+++ src/components/shared-components/add-button/add-button.js
@@ -7,15 +7,3 @@ const AddButton = ({ onClick }) => (
     </button>
 );
 export default AddButton;
-
-
-/* SECOND OPTION */
-
-// function AddButton(props) {
-//     return (
-//         <button className="fab" onClick={props.onClick}>
-//             <i className="fa fa-plus" aria-hidden="true"></i>
-//         </button>
-//     );
-// }
-// export default AddButton;
